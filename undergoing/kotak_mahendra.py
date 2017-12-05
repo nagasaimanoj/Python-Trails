19ffb1c14f18e38fcb7535bc738c4a09a562f758
nagasaimanojkumar G <saimanoj@KGFSL.COM>
2017-12-05 12:11:02 +0530

    future predection upto 5 years

--- undergoing/kotak_mahendra.py
+++ undergoing/kotak_mahendra.py
@@ -9,14 +9,18 @@ csv_reader = csv.reader(csv_file)
 csv_data = np.array(list(csv_reader))
 
 x_data = np.array(csv_data[..., :1], dtype=int)
-y_data = np.array(csv_data[..., 1:], dtype=float)
-
-y_data.shape = (1, 8)
-y_data = y_data[0]
+y_data = np.array(csv_data[..., 1:], dtype=float).T[0]
 
 regr = linear_model.LinearRegression()
 regr.fit(x_data, y_data)
 
 plt.scatter(x_data, y_data)
+
+x_data = np.array(np.append(x_data, np.arange(
+    max(x_data) + 1, max(x_data) + 6)))
+
+x_data.shape = (len(x_data), 1)
+
+plt.scatter(x_data, regr.predict(x_data))
 plt.plot(x_data, regr.predict(x_data))
 plt.show()
