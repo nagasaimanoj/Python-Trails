e5f88deb97aa65df5fdfda6025b2e4bb09210e11
nagasaimanojkumar G <saimanoj@KGFSL.COM>
2017-12-05 11:12:43 +0530

    regression working

--- undergoing/kotak_mahendra.py
+++ undergoing/kotak_mahendra.py
@@ -6,31 +6,17 @@ from sklearn import linear_model
 
 csv_file = open("C:\\Users\\saimanoj\\Downloads\\data.csv")
 csv_reader = csv.reader(csv_file)
-csv_data = list(csv_reader)
+csv_data = np.array(list(csv_reader))
 
-x_data = []
-y_data = []
+x_data = np.array(csv_data[..., :1], dtype=int)
+y_data = np.array(csv_data[..., 1:], dtype=float)
 
-for x in csv_data:
-    x_data.append(x[0])
-    y_data.append(x[1])
-
-plt.scatter(x_data, y_data)
+y_data.shape = (1, 8)
+y_data = y_data[0]
 
 regr = linear_model.LinearRegression()
-
-x_data = np.array(x_data, ndmin=2)
-
 regr.fit(x_data, y_data)
 
-print("trained")
-
-print(x_data, y_data)
-
-y_data = regr.predict(x_data)
-
-print("values re-generated")
-
-plt.plot(x_data, y_data)
-
+plt.scatter(x_data, y_data)
+plt.plot(x_data, regr.predict(x_data))
 plt.show()
