84bbaf6f2aa4d874f40f86632ca3dbc3f8af1693
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2017-12-07 19:43:02 +0530

    improvements

--- Regression/Poisson_Regression/FootBall_Goal_Predections.py
+++ Regression/Poisson_Regression/FootBall_Goal_Predections.py
@@ -3,18 +3,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import poisson
 
+# collecting foot ball match data from a CSV file
 match_data = pd.read_csv(
     "C:\\Users\\saimanoj\\Downloads\\E0.csv")[['FTHG', 'FTAG']]
 
+# plotting a histogram based on actual data
 plt.hist(match_data.values, range(10), normed=True,
          color=['#77AA77', '#AAAAAA'], label=['Home', 'Away'])
 
-plt.plot(np.arange(0.5, 10.5, 1), poisson.pmf(range(10), match_data.mean()[0]),
+# generating an array of values for x-axis
+x_axis = np.arange(0.5, 10.5, 1)
+
+# plotting a line chart for predected home runs based on actual data
+plt.plot(x_axis, poisson.pmf(range(10), match_data.mean()[0]),
          marker='o', label="Home", color='#FF88c0')
 
-plt.plot(np.arange(0.5, 10.5, 1), poisson.pmf(range(10), match_data.mean()[1]),
+# plotting a line chart for predected away runs based on actual data
+plt.plot(x_axis, poisson.pmf(range(10), match_data.mean()[1]),
          marker='o', label="Away", color='#1e88e5')
 
+# decorating the chart with title and dimension details
 plt.title("Number of Goals per Match", size=15, fontweight='bold')
 plt.xlabel("Goals per Match")
 plt.ylabel("Goal Probability")
--- Regression/Poisson_Regression/poission_regression_simplest.py
+++ Regression/Poisson_Regression/poission_regression_simplest.py
@@ -2,19 +2,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import poisson
 
+# collecting goal_count data per match
 match_data = np.array([0, 0, 1, 2, 2, 1, 1, 3, 1, 2, 2, 2, 0, 1, 0, 1, 1, 1, 1, 1, 3, 1, 1, 0, 2, 1, 1, 1, 3, 0, 2, 1, 1, 4, 1, 1, 0, 2, 2, 0, 1, 3, 1, 3, 4, 4, 4, 1, 1, 3, 3, 1, 5, 4, 1, 1, 2, 1, 0, 2, 1, 0, 1, 1, 2, 1, 0, 0, 1, 2, 3, 6, 3, 0, 1, 2, 1, 0, 3, 0, 0, 0, 2, 0, 3, 2, 0, 1, 4, 1, 2, 0, 2, 1, 1, 1, 0, 2, 0, 3, 1, 3, 5, 1, 1, 1, 2, 1, 6, 1, 1, 1, 1, 0, 0, 3, 3, 2, 0, 4, 1, 2, 1, 2, 2, 5, 3, 1, 1, 0, 3, 1, 2, 2, 5, 3, 1, 4, 1, 1, 3, 3, 3, 4, 3, 3, 1, 2, 1, 1, 1, 2, 1, 2, 0, 0, 0, 3, 3, 1, 0, 3, 2, 1, 0, 1, 1, 2, 2, 0, 1, 1, 3, 0, 0, 3, 1, 1, 4, 1, 2, 4, 4, 1, 1, 2, 1,
                        0, 2, 1, 3, 2, 0, 2, 3, 0, 3, 1, 2, 2, 1, 3, 0, 1, 0, 4, 0, 3, 4, 1, 2, 0, 2, 2, 1, 1, 2, 2, 2, 3, 1, 0, 1, 1, 1, 0, 2, 0, 1, 0, 3, 0, 6, 2, 1, 1, 2, 1, 0, 2, 2, 2, 2, 0, 1, 0, 2, 1, 2, 0, 3, 1, 2, 1, 1, 2, 4, 3, 3, 3, 1, 2, 3, 3, 0, 0, 3, 1, 0, 3, 3, 2, 2, 2, 1, 4, 1, 0, 3, 2, 1, 1, 2, 0, 1, 2, 2, 3, 0, 0, 1, 2, 0, 1, 2, 1, 2, 3, 2, 4, 2, 3, 1, 1, 3, 0, 1, 4, 0, 1, 4, 0, 3, 2, 3, 0, 3, 2, 4, 1, 2, 0, 1, 4, 2, 2, 0, 0, 1, 4, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 0, 1, 2, 2, 0, 3, 5, 1, 2, 0, 3, 0, 1, 0, 2, 2, 1, 1, 0, 4, 2, 0, 4, 2, 3, 0, 1, 3, 1, 5, 1, 1, 3, 2, 0, 2, 0])
 
+# storing unique goal_counts in x_data and storing predicted values in pred_data
 x_data = np.unique(match_data)
 pred_data = poisson.pmf(x_data, match_data.mean())
 
+# counting the No. of times an unique goal_count repeats and storing them in num_count
 num_count = []
 for i in x_data:
     num_count = np.append(num_count, np.count_nonzero(match_data == i))
 
+# replacing count values with their corrosponding occurances in the whole set
 num_count = num_count / sum(num_count)
 
-plt.plot(x_data, num_count, marker='*')
+# plotting a dotted graph based on existing data
+plt.scatter(x_data, num_count, marker='*')
+
+# plotting a graph based on predected data
 plt.plot(x_data, pred_data, marker='o')
 
 plt.show()
