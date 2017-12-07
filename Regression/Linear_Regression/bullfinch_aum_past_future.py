8e1b998efe53f3171ca5ca0982d4888d35364ce3
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2017-12-07 17:12:00 +0530

    training for every iteration

--- Regression/Linear_Regression/bullfinch_aum_past_future.py
+++ Regression/Linear_Regression/bullfinch_aum_past_future.py
@@ -5,10 +5,10 @@ years = [[2012], [2013], [2014], [2015], [2016], [2017]]
 aum_per_year = [0, 0, 0, 5166063.898, 404537126.8, 3322383541]
 
 reg = linear_model.LinearRegression()
-reg.fit(years, aum_per_year)
 
 for i in range(1, 6):
     next_year = [[int(max(years)[0]) + 1]]
+    reg.fit(years, aum_per_year)
 
     years += next_year
     aum_per_year += list(reg.predict(next_year))
