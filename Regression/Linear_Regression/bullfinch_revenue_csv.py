1c1faa4ad86ffceb511b59cacb5fe8167dd24e89
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2017-12-07 15:58:01 +0530

    polished

--- Regression/Linear_Regression/bullfinch_revenue_csv.py
+++ Regression/Linear_Regression/bullfinch_revenue_csv.py
@@ -1,18 +1,14 @@
 import csv
-
 import matplotlib.pyplot as plt
 from sklearn import linear_model
+import numpy as np
 
 csv_file = open("C:\\Users\\saimanoj\\Downloads\\revenue.csv")
 csv_reader = csv.reader(csv_file)
-csv_data = list(csv_reader)
+csv_data = np.array(list(csv_reader))
 
-x_data = []
-y_data = []
-
-for x in csv_data:
-    x_data.append(x[0])
-    y_data.append(x[1])
+x_data = csv_data[..., 0]
+y_data = csv_data[..., 1]
 
 plt.plot(x_data, y_data, marker='o')
 
