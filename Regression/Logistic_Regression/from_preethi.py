bd9de86224151e3abb6ba6a9a9895530ec4c09ee
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2017-12-08 17:28:36 +0530

    hided technical parts

--- Regression/Logistic_Regression/from_preethi.py
+++ Regression/Logistic_Regression/from_preethi.py
@@ -1,39 +1,36 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.metrics import confusion_matrix, roc_curve, roc_auc_score
+from sklearn.metrics import confusion_matrix, roc_curve
 from sklearn.linear_model import LogisticRegression
 
 Diabetes = pd.read_csv("C:\\Users\\saimanoj\\Downloads\\diabetes.csv")
 
-table1 = np.mean(Diabetes, axis=0)
-table2 = np.std(Diabetes, axis=0)
+# table1 = np.mean(Diabetes, axis=0)
+# table2 = np.std(Diabetes, axis=0)
 
 inputData = Diabetes.iloc[:, :8]
 outputData = Diabetes.iloc[:, 8]
 
-logit1 = LogisticRegression()
-logit1.fit(inputData, outputData)
-logit1.score(inputData, outputData)
+log_model = LogisticRegression()
+log_model.fit(inputData, outputData)
+# log_model.score(inputData, outputData)
 
-trueInput = Diabetes.ix[Diabetes['Outcome'] == 1].iloc[:, :8]
-trueOutput = Diabetes.ix[Diabetes['Outcome'] == 1].iloc[:, 8]
+# trueInput = Diabetes.ix[Diabetes['Outcome'] == 1].iloc[:, :8]
+# trueOutput = Diabetes.ix[Diabetes['Outcome'] == 1].iloc[:, 8]
 
-np.mean(logit1.predict(trueInput) == trueOutput)
+# np.mean(log_model.predict(trueInput) == trueOutput)
 
-falseInput = Diabetes.ix[Diabetes['Outcome'] == 0].iloc[:, :8]
-falseOutput = Diabetes.ix[Diabetes['Outcome'] == 0].iloc[:, 8]
+# falseInput = Diabetes.ix[Diabetes['Outcome'] == 0].iloc[:, :8]
+# falseOutput = Diabetes.ix[Diabetes['Outcome'] == 0].iloc[:, 8]
 
-np.mean(logit1.predict(falseInput) == falseOutput)
+# np.mean(log_model.predict(falseInput) == falseOutput)
 
-confusion_matrix(logit1.predict(inputData), outputData)
+# confusion_matrix(log_model.predict(inputData), outputData)
 
-fpr, tpr, _ = roc_curve(logit1.predict(inputData),
-                        outputData, drop_intermediate=False)
+fpr, tpr, _ = roc_curve(log_model.predict(inputData), outputData)
 
 plt.plot(fpr, tpr)
 
 plt.plot([0, 1], [0, 1])
 plt.show()
-
-print(roc_auc_score(logit1.predict(inputData), outputData))
