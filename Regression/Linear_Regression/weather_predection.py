109b8225c3c5339865925ecff76bab93d6b920dd
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2017-12-11 19:33:37 +0530

    cleared useless

--- Regression/Linear_Regression/weather_predection.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-import json
-
-url = urllib.request.urlopen(
-    "http://maps.googleapis.com/maps/api/geocode/json?address=google")
-data = json.loads(url.read().decode())
-
-data = dict(data)
