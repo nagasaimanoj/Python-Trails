19b21314d2e2dd998e917c096569c795b08ffc3e
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2017-12-11 14:37:55 +0530

    simplified

---
+++ Regression/Linear_Regression/linear_regression_multiple_simplified.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+import numpy as np
+
+# training data
+training_input = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [9, 10, 11], [12, 13, 14],
+                  [15, 16, 17], [18, 19, 20], [21, 22, 23], [24, 25, 26], [27, 28, 29]]
+training_output = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# training the model using linear regression
+reg = linear_model.LinearRegression()
+reg.fit(training_input, training_output)
+
+# generating a test data set
+test_input = training_input + [[30, 31, 32], [33, 34, 35], [36, 37, 38]]
+
+# plotting graphs using actual data & our predections
+plt.plot(range(len(training_input)), training_output, marker="o")
+plt.plot(range(len(test_input)), reg.predict(test_input), marker="*")
+plt.show()
