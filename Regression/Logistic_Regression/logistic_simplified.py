1898b5bc5c85b0fcd4c615e3cbf3d9ffd3510e48
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2017-12-11 14:24:24 +0530

    simplified

--- Regression/Logistic_Regression/logistic_simplified.py
+++ Regression/Logistic_Regression/logistic_simplified.py
@@ -13,16 +13,3 @@ log_model.fit(inputData, outputData)
 training_data_accuracy = np.mean(log_model.predict(inputData) == outputData)
 
 print("training data accuracy =", training_data_accuracy)
-
-test_input = [[1, 189, 60, 23, 846, 30.1, 0.398, 59],
-              [5, 166, 72, 19, 175, 25.8, 0.587, 51],
-              [3, 126, 88, 41, 235, 39.3, 0.704, 27],
-              [8, 99, 84, 0, 0, 35.4, 0.388, 50],
-              [1, 97, 66, 15, 140, 23.2, 0.487, 22]]
-test_output = [1, 1, 0, 0, 0]
-
-pred_results = log_model.predict(test_input)
-
-test_data_accuracy = np.mean(test_output == pred_results)
-
-print("test data accuracy =", test_data_accuracy)
