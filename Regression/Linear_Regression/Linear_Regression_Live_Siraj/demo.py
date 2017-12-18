dc81897fdb787cc5294084b9749257d558aa7259
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2017-12-18 18:01:39 +0530

    simplified : replaced looping through array with direct manuplation

--- Regression/Linear_Regression/Linear_Regression_Live_Siraj/demo.py
+++ Regression/Linear_Regression/Linear_Regression_Live_Siraj/demo.py
@@ -1,29 +1,19 @@
-from numpy import genfromtxt
+from numpy import genfromtxt, mean
 
-
-points = genfromtxt(
-    "C:\\Users\\saimanoj\\Downloads\\data.csv", delimiter=",")
+points = genfromtxt("C:\\Users\\saimanoj\\Downloads\\data.csv", delimiter=",")
 
 b, m = 0, 0
 num_iterations = len(points) * 10
 learning_rate = 1 / (num_iterations * 10)
 
 for i in range(num_iterations):
-    b_gradient = 0
-    m_gradient = 0
 
-    for i in range(0, len(points)):
-        x, y = points[i, 0], points[i, 1]
+    avg_change = mean(2 / len(points) *
+                      (points[:, 1] - (m * points[:, 0]) + b))
 
-        avg_change = (2 / len(points)) * (y - (m * x) + b)
-
-        b_gradient += avg_change
-        m_gradient += avg_change * x
-
-    b += b_gradient * learning_rate
-    m += m_gradient * learning_rate
+    b += avg_change * learning_rate
+    m += (avg_change * sum(points[:, 0])) * learning_rate
 
 print("b =", b)
 print("m =", m)
-print("error =", sum(
-    (points[:, 1] - (m * points[:, 0] + b)) ** 2) / len(points))
+print("error =", mean((points[:, 1] - (m * points[:, 0] + b)) ** 2))
