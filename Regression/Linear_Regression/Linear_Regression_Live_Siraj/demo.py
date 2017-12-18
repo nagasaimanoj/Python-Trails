ff65b04ea56a9587695f7f0730a1077ef1714c53
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2017-12-18 17:40:54 +0530

    replaced for-loop with direct manuplation to manuplate array

--- Regression/Linear_Regression/Linear_Regression_Live_Siraj/demo.py
+++ Regression/Linear_Regression/Linear_Regression_Live_Siraj/demo.py
@@ -11,24 +11,19 @@ learning_rate = 0.0001
 for i in range(num_iterations):
     b_gradient = 0
     m_gradient = 0
-    N = float(len(points))
 
     for i in range(0, len(points)):
         x, y = points[i, 0], points[i, 1]
 
-        avg_change_in_value = (2 / N) * (y - (m * x) + b)
+        avg_change = (2 / len(points)) * (y - (m * x) + b)
 
-        b_gradient += -avg_change_in_value
-        m_gradient += -avg_change_in_value * x
+        b_gradient += -avg_change
+        m_gradient += -avg_change * x
 
     b -= learning_rate * b_gradient
     m -= learning_rate * m_gradient
 
-total_error = 0
-for i in range(0, len(points)):
-    x, y = points[i, 0], points[i, 1]
-    total_error += (y - (m * x + b)) ** 2
-
 print("b =", b)
 print("m =", m)
-print("error =", total_error / float(len(points)))
+print("error =", sum(
+    (points[:, 1] - (m * points[:, 0] + b)) ** 2) / float(len(points)))
