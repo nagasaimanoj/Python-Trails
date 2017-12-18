bf4b1636debb846e902b4c4950b2b966c6dd8427
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2017-12-18 17:50:35 +0530

    removed hard coaded values to adapt to data set size

--- Regression/Linear_Regression/Linear_Regression_Live_Siraj/demo.py
+++ Regression/Linear_Regression/Linear_Regression_Live_Siraj/demo.py
@@ -5,8 +5,8 @@ points = genfromtxt(
     "C:\\Users\\saimanoj\\Downloads\\data.csv", delimiter=",")
 
 b, m = 0, 0
-num_iterations = 1000
-learning_rate = 0.0001
+num_iterations = len(points) * 10
+learning_rate = 1 / (num_iterations * 10)
 
 for i in range(num_iterations):
     b_gradient = 0
@@ -17,13 +17,13 @@ for i in range(num_iterations):
 
         avg_change = (2 / len(points)) * (y - (m * x) + b)
 
-        b_gradient += -avg_change
-        m_gradient += -avg_change * x
+        b_gradient += avg_change
+        m_gradient += avg_change * x
 
-    b -= learning_rate * b_gradient
-    m -= learning_rate * m_gradient
+    b += b_gradient * learning_rate
+    m += m_gradient * learning_rate
 
 print("b =", b)
 print("m =", m)
 print("error =", sum(
-    (points[:, 1] - (m * points[:, 0] + b)) ** 2) / float(len(points)))
+    (points[:, 1] - (m * points[:, 0] + b)) ** 2) / len(points))
