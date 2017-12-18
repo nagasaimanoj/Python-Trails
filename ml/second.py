6313abc793bfc3b1fd5b4c14fe136cd1215d9dde
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2017-12-18 15:47:21 +0530

    deleted useless programs

--- ml/second.py
+++
@@ -1,8 +0,0 @@
-import tensorflow.contrib.learn as skflow
-from sklearn import datasets, metrics
-
-iris = datasets.load.iris()
-classifier = skflow.TensorFlowLinearClassifier(n_classes=3)
-classifier.fit(iris.data, iris.target)
-score = metrics.accuracy_score(iris.target, classifier.predict(iris.data))
-print("Accuracy= %f" % score)
