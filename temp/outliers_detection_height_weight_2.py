02adb32c4a201ae42efe44095f30a792e4e4752d
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2017-12-27 15:50:34 +0530

    simplified

---
+++ temp/outliers_detection_height_weight_2.py
@@ -0,0 +1,17 @@
+from os.path import dirname
+
+from matplotlib.pyplot import plot, show
+from numpy import genfromtxt
+from sklearn.svm import OneClassSVM
+
+DATA_SET_1 = genfromtxt(dirname(__file__) + "\\" + "file_1.csv", delimiter=",")
+
+DATA_SET_1_PRED = OneClassSVM(nu=0.05, kernel="rbf", gamma=0.1).fit(
+    DATA_SET_1).predict(DATA_SET_1)
+
+NORMAL = DATA_SET_1[DATA_SET_1_PRED == 1]
+ABNORMAL = DATA_SET_1[DATA_SET_1_PRED == -1]
+
+plot(NORMAL[:, 0], NORMAL[:, 1], "bx")
+plot(ABNORMAL[:, 0], ABNORMAL[:, 1], "ro")
+show()
