d9ddc367f88c1fb941b3f17ac78481b2d68b4169
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2017-12-28 12:13:53 +0530

    gamma, nu values taking directly from SVM_MODEL

--- Anomaly_Detection/OneClassSVM.py
+++ Anomaly_Detection/OneClassSVM.py
@@ -7,10 +7,10 @@ from sklearn.svm import OneClassSVM
 DATA_SET_1 = genfromtxt(dirname(__file__) + "\\" + "women.csv", delimiter=",")
 DATA_SET_1_LENGTH = len(DATA_SET_1[:, 0])
 
-ERROR_PERC = 0.05
+NU_VAL = 0.05
 GAMMA_VAL = 7000000
 
-SVM_MODEL = OneClassSVM(nu=1 / (DATA_SET_1_LENGTH * ERROR_PERC),
+SVM_MODEL = OneClassSVM(nu=1 / (DATA_SET_1_LENGTH * NU_VAL),
                         gamma=1 / (DATA_SET_1_LENGTH * GAMMA_VAL))
 
 SVM_MODEL.fit(DATA_SET_1)
@@ -21,5 +21,5 @@ ABNORMAL = DATA_SET_1[DATA_SET_1_PRED == -1]
 
 plot(NORMAL[:, 0], NORMAL[:, 1], "bx")
 plot(ABNORMAL[:, 0], ABNORMAL[:, 1], "ro")
-title("gamma = " + str(GAMMA_VAL) + ", nu = " + str(ERROR_PERC))
+title("gamma = " + str(SVM_MODEL.gamma) + ", nu = " + str(SVM_MODEL.nu))
 show()
