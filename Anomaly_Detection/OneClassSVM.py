69450c532c9fbdaa5d2125bf6804ab0754fecbd0
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2017-12-28 17:29:17 +0530

    parameter passing simplified

--- Anomaly_Detection/OneClassSVM.py
+++ Anomaly_Detection/OneClassSVM.py
@@ -1,17 +1,16 @@
 from os.path import dirname
 
 from matplotlib.pyplot import plot, show, title
-from numpy import genfromtxt
-from sklearn.svm import OneClassSVM
+from numpy import genfromtxt, array
+from sklearn.svm import SVC, OneClassSVM
 
-DATA_SET_1 = genfromtxt(dirname(__file__) + "\\" + "women.csv", delimiter=",")
-DATA_SET_1_LENGTH = len(DATA_SET_1[:, 0])
+DATA_SET_1 = array(genfromtxt(dirname(__file__) +
+                              "\\" + "women.csv", delimiter=","))
 
-NU_VAL = 0.05
-GAMMA_VAL = 7000000
+NU_VAL = 1 / 25
+GAMMA_VAL = 1 / 3500000000
 
-SVM_MODEL = OneClassSVM(nu=1 / (DATA_SET_1_LENGTH * NU_VAL),
-                        gamma=1 / (DATA_SET_1_LENGTH * GAMMA_VAL))
+SVM_MODEL = OneClassSVM(nu=NU_VAL, gamma=GAMMA_VAL)
 
 SVM_MODEL.fit(DATA_SET_1)
 DATA_SET_1_PRED = SVM_MODEL.predict(DATA_SET_1)
