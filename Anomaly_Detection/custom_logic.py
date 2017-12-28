a85ef476974a3cd35aefa72166c1773da3903002
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2017-12-28 12:25:10 +0530

    Doc_String added and variable names changed

--- Anomaly_Detection/custom_logic.py
+++ Anomaly_Detection/custom_logic.py
@@ -1,25 +1,31 @@
+"""
+this will find mean value and standard deviation
+    and finds minimum values and maximum values based on those.
+any point greater then maximum or lesser then minimum will be an outlier
+"""
+
 from matplotlib.pyplot import plot, show
 from numpy import mean, std
 
-height = [58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71,
+HEIGHT = [58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71,
           72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86]
-weight = [115, 117, 120, 123, 126, 129, 132, 135, 139, 142, 146, 150, 154, 159,
+WEIGHT = [115, 117, 120, 123, 126, 129, 132, 135, 139, 142, 146, 150, 154, 159,
           164, 164, 168, 171, 175, 178, 182, 185, 188, 192, 195, 199, 202, 206, 209]
 
-height_mean = mean(height)
-height_std = std(height)
-height_upper_bound = height_mean + height_std
-height_lower_bound = height_mean - height_std
+HEIGHT_MEAN = mean(HEIGHT)
+HEIGHT_STD = std(HEIGHT)
+HEIGHT_UPPER_BOUND = HEIGHT_MEAN + HEIGHT_STD
+HEIGHT_LOWER_BOUND = HEIGHT_MEAN - HEIGHT_STD
 
-weight_mean = mean(weight)
-weight_std = std(weight)
-weight_upper_bound = weight_mean + weight_std
-weight_lower_bound = weight_mean - weight_std
+WEIGHT_MEAN = mean(WEIGHT)
+WEIGHT_std = std(WEIGHT)
+WEIGHT_UPPER_BOUND = WEIGHT_MEAN + WEIGHT_std
+WEIGHT_LOWER_BOUND = WEIGHT_MEAN - WEIGHT_std
 
-for i in range(len(height)):
-    if((height_lower_bound < height[i] < height_upper_bound)or(weight_lower_bound < weight[i] < weight_upper_bound)):
-        plot(height[i], weight[i], 'bo')
+for i in range(len(HEIGHT)):
+    if((HEIGHT_LOWER_BOUND < HEIGHT[i] < HEIGHT_UPPER_BOUND)or(WEIGHT_LOWER_BOUND < WEIGHT[i] < WEIGHT_UPPER_BOUND)):
+        plot(HEIGHT[i], WEIGHT[i], 'bo')
     else:
-        plot(height[i], weight[i], 'rx')
+        plot(HEIGHT[i], WEIGHT[i], 'rx')
 
 show()
