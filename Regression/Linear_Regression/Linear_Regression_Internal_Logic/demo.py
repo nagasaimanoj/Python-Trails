e8d8f30d1e9175b8656356c27a14b8df314ec329
naga gowtham giddaluru <nagasai.g9@gmail.com>
2018-01-08 07:50:13 +0530

    output slope name changed

--- Regression/Linear_Regression/Linear_Regression_Internal_Logic/demo.py
+++ Regression/Linear_Regression/Linear_Regression_Internal_Logic/demo.py
@@ -1,6 +1,7 @@
-# genfromtxt will read csv as array & mean will calculate average
+# dirname gives current file path
 from os.path import dirname
 
+# genfromtxt will read csv as array & mean will calculate average
 from numpy import genfromtxt, mean
 
 # reading csv file as an array
@@ -19,16 +20,16 @@ learning_rate = 1 / (data_num_iterations * 10)
 for i in range(data_num_iterations):
     # calculating average change in values when compared with predected and actual values
     avg_change = mean(2 / len(DATA_POINTS) *
-                      (DATA_POINTS[:, 1] - (data_slope * DATA_POINTS[:, 0]) + data_intercept))
+                      (DATA_POINTS[:, 1] - data_slope * DATA_POINTS[:, 0] + data_intercept))
 
     # using that average change to adjust intercept
     data_intercept += avg_change * learning_rate
 
     # using that average change to adjust slope
-    data_slope += (avg_change * sum(DATA_POINTS[:, 0])) * learning_rate
+    data_slope += sum(DATA_POINTS[:, 0]) * avg_change * learning_rate
 
 # printing output values one by one
 print("intercept =", data_intercept)
-print("m =", data_slope)
+print("slope =", data_slope)
 print("error =", mean(
     (DATA_POINTS[:, 1] - (data_slope * DATA_POINTS[:, 0] + data_intercept)) ** 2))
