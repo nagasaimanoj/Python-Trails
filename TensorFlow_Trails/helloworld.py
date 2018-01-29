ce7635b9c21b51f6d46865756d3b5644b8700ea5
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-01-29 19:02:26 +0530

    import style changed

--- TensorFlow_Trails/helloworld.py
+++ TensorFlow_Trails/helloworld.py
@@ -1,4 +1,5 @@
-from tensorflow import constant, Session
+from tensorflow import (Session,  # to start TF session
+                        constant)  # to declare constant variables
 
 # creating Session to run a s'tring. this will print it's value
 print(Session().run(constant("Hello, TensorFlow!")))
