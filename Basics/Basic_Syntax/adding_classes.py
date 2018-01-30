11329cbf734430db2ecd8e2bf7e1ffdaf6cd9076
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-01-30 12:16:23 +0530

    class name changed

--- Basics/Basic_Syntax/adding_classes.py
+++ Basics/Basic_Syntax/adding_classes.py
@@ -1,14 +1,16 @@
-class Vector2D:
+class someClass:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
     def __add__(self, other):
-        return Vector2D(self.x + other.x, self.y + other.y)
+        return someClass(self.x + other.x, self.y + other.y)
 
 
-first = Vector2D(5, 7)
-second = Vector2D(3, 9)
+first = someClass(5, 7)
+second = someClass(3, 9)
+
 result = first + second
+
 print(result.x)
 print(result.y)
