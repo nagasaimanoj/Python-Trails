262b62568a97af2e05fe8c2c5deaba702c995deb
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-01-30 12:16:49 +0530

    simplified

--- Basics/Basic_Programs/Fizz_Buzz.py
+++ Basics/Basic_Programs/Fizz_Buzz.py
@@ -1,11 +1,9 @@
-def print_Fizz_Buzz(num):
-    for i in range(1, num + 1):
-        text = str(i)
-        if(i % 3 == 0):
-            text += " - Fizz"
-        if(i % 5 == 0):
-            text += " - Buzz"
-        print(text)
+num = int(input("Enter range : "))
 
-
-print_Fizz_Buzz(int(input("Enter range : ")))
+for i in range(1, num + 1):
+    text = str(i)
+    if(i % 3 == 0):
+        text += " - Fizz"
+    if(i % 5 == 0):
+        text += " - Buzz"
+    print(text)
