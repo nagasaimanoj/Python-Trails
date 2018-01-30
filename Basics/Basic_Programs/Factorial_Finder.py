58ed7cc4b10cb6e4f16b6ca81394bfefca6e1bb9
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-01-30 12:17:03 +0530

    simplified

--- Basics/Basic_Programs/Factorial_Finder.py
+++ Basics/Basic_Programs/Factorial_Finder.py
@@ -1,14 +1,9 @@
-def factorial(num):
-    if(num > 1):
-        return (num * factorial(num - 1))
-    else(num=1):
-        return 1
-
-
-print("factorial finder")
 num = int(input("enter a number to find it's factorial : "))
 
-if(num < 0):
-    print(str(1))
+result = 1
 
-print(str(factorial(num)))
+if(num > 1):
+    for i in range(1, num + 1):
+        result *= i
+
+print(result)
