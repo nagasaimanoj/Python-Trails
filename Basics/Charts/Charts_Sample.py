a5aef3d922b5d2fff8bc7376e71efb1a02caa0f2
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-02-19 16:58:47 +0530

    shown as single graph & with legend()

--- Basics/Charts/Charts_Sample.py
+++ Basics/Charts/Charts_Sample.py
@@ -1,21 +1,19 @@
-from matplotlib.pyplot import (grid, plot, show, title, xlabel, ylabel)
+from matplotlib.pyplot import (grid, legend, plot, scatter, show, title,
+                               xlabel, ylabel)
 
 x = [1, 2, 3, 4, 5, 6]
 y = [20, 3, 40, 5, 60, 7]
 z = [1, 5, 3, 7, 11, 9]
 
-xlabel("X data")
-ylabel("Y data")
-title("X vs Y")
-grid(True)
+plot(x, y, label="x to y")
+plot(x, z, label="x to z")
+scatter(x, z, label="x to z")
 
-plot(x, y)
-show()
-
+title("X vs Z")
 xlabel("X data")
 ylabel("Z data")
-title("X vs Z")
-grid(True)
 
-plot(x, z)
+grid(True)
+legend()
+
 show()
