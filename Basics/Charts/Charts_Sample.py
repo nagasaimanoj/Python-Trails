8ce68d57e8a093230676c5ad4fe505a54db579aa
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-02-19 16:23:22 +0530

    re-organised

--- Basics/Charts/Charts_Sample.py
+++ Basics/Charts/Charts_Sample.py
@@ -2,7 +2,6 @@ from matplotlib.pyplot import (grid, plot, show, title, xlabel, ylabel)
 
 x = [1, 2, 3, 4, 5, 6]
 y = [20, 3, 40, 5, 60, 7]
-z = [1, 5, 3, 7, 11, 9]
 
 xlabel("X data")
 ylabel("Y data")
@@ -12,6 +11,8 @@ grid(True)
 plot(x, y)
 show()
 
+
+z = [1, 5, 3, 7, 11, 9]
 xlabel("X data")
 ylabel("Z data")
 title("X vs Z")
