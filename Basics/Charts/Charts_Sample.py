f879b78d250bf30a5095bd6500a7433fa1547a00
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-02-19 17:00:31 +0530

    added colors, changed line to scatter

--- Basics/Charts/Charts_Sample.py
+++ Basics/Charts/Charts_Sample.py
@@ -5,9 +5,8 @@ x = [1, 2, 3, 4, 5, 6]
 y = [20, 3, 40, 5, 60, 7]
 z = [1, 5, 3, 7, 11, 9]
 
-plot(x, y, label="x to y")
-plot(x, z, label="x to z")
-scatter(x, z, label="x to z")
+plot(x, y, label="x to y", color="green")
+scatter(x, z, label="x to z", color="red")
 
 title("X vs Z")
 xlabel("X data")
