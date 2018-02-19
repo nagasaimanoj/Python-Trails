87564066c323c14d4689305c658c5bb94e449661
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-02-19 17:02:32 +0530

    grid removed, title & lables changed

--- Basics/Charts/Charts_Sample.py
+++ Basics/Charts/Charts_Sample.py
@@ -1,4 +1,4 @@
-from matplotlib.pyplot import (grid, legend, plot, scatter, show, title,
+from matplotlib.pyplot import (legend, plot, scatter, show, title,
                                xlabel, ylabel)
 
 x = [1, 2, 3, 4, 5, 6]
@@ -8,11 +8,9 @@ z = [1, 5, 3, 7, 11, 9]
 plot(x, y, label="x to y", color="green")
 scatter(x, z, label="x to z", color="red")
 
-title("X vs Z")
+title("X vs Y & X vs Z")
 xlabel("X data")
-ylabel("Z data")
+ylabel("Y & Z data")
 
-grid(True)
 legend()
-
 show()
