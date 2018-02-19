ce5daadc6ee2b499c9ebd5148baa57091c2852af
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-02-19 17:14:49 +0530

    grid is back, colors changed

--- Basics/Charts/Charts_Sample.py
+++ Basics/Charts/Charts_Sample.py
@@ -1,16 +1,17 @@
-from matplotlib.pyplot import (legend, plot, scatter, show, title,
-                               xlabel, ylabel)
+from matplotlib.pyplot import (
+    grid, legend, plot, scatter, show, title, xlabel, ylabel)
 
 x = [1, 2, 3, 4, 5, 6]
 y = [20, 3, 40, 5, 60, 7]
 z = [1, 5, 3, 7, 11, 9]
 
-plot(x, y, label="x to y", color="green")
-scatter(x, z, label="x to z", color="red")
+plot(x, y, color="#003B46", label="x to y")
+scatter(x, z, color="#F52549", label="x to z")
 
 title("X vs Y & X vs Z")
 xlabel("X data")
 ylabel("Y & Z data")
 
 legend()
+grid()
 show()
