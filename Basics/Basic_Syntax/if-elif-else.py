453bf572ebba946fba4409dfb3da5d5add6f49f9
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-03-18 13:19:58 +0530

    checking working conditions for if-else

---
+++ Basics/Basic_Syntax/if-elif-else.py
@@ -0,0 +1,34 @@
+# using string
+direction = "Right"
+
+if(direction == "Right"): 
+    print("going Right")
+elif(direction == "Left"): 
+    print("going Left")
+else: 
+    print("Unable to Navigate")
+
+
+# using numbers
+value = 0
+
+if(value):
+    print("non-zero will be True")
+else:
+    print("only 0 is False")
+
+# using boolean
+bool_val = True
+
+if(bool_val):
+    print("True is acceptable")
+else:
+    print("False is not acceptable")
+
+# using list
+a = [1]
+
+if(a):
+    print("non-empty list is True")
+else:
+    print("empty list will be False")
