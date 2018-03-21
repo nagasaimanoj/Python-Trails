3ee82c777cf6416e6f1d53d039fe98e97541bc1b
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-03-21 08:04:04 +0530

    used different methods for add and delete

--- Web Dev/Flask/Sample_List/index.py
+++ Web Dev/Flask/Sample_List/index.py
@@ -1,18 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
 userList = []
 
 
-@app.route("/", methods=['POST', 'GET'])
-def hello():
-
-    if request.method == 'POST':
-        uname = request.form['uname']
-        userList.append(uname)
-
-    return render_template('home.html', content=userList)
+@app.route('/', methods=['GET'])
+def home_page():
+    return render_template('home.html', userList=userList)
 
 
-app.run()
+@app.route('/addItem', methods=['POST'])
+def addItem():
+    uname = request.form['uname']
+    userList.append(uname)
+    return redirect('/')
+
+
+@app.route('/deleteItem', methods=['GET'])
+def deleteItem():
+    name = request.args.get('name')
+    userList.remove(name)
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
