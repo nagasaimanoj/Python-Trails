9c86c1f3736b164f8b8678ba51d67c0174bdee1e
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-04-01 16:42:27 +0530

    total example code changed

--- Basics/Basic_Syntax/Decorator.py
+++ Basics/Basic_Syntax/Decorator.py
@@ -1,22 +1,17 @@
-def dashes(func):
-    def someFun():
-        print("-=-=-=-=-=-")
-        func()
-        print("-=-=-=-=-=-")
-    return someFun
+# this functoin is decorator
+def someXlogic(someFunc):
+        # this has to return an object or function. so defining one
+        def someOtherFunc(x, y):
+                print("prior to actual logic")
+                someFunc(x, y)
+                print("after my actual logic")
+                # returning an object
+        return someOtherFunc
 
+# defining a functoin with decorator. this will run our functoin with decorators rules
+@someXlogic
+def myLogic(a, b):
+        print(a + b)
 
-def someY(func):
-    def someFun():
-        print("**********")
-        func()
-        print("**********")
-    return someFun
-
-
-@someY
-def someX():
-    print("someX")
-
-
-someX()
+# calling in normal way. but decorator will get involved
+myLogic(1, 2)
