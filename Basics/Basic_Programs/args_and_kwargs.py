eba08138cd0f279ba67febbaab4fabdfeb5838ae
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-04-01 07:52:26 +0530

    args - kwargs tried

---
+++ Basics/Basic_Programs/args_and_kwargs.py
@@ -0,0 +1,7 @@
+def someLogic(var1, var2, *vars, **key_args):
+    print(var1)
+    print(var2)
+    print(vars)
+    print(key_args)
+
+someLogic(1, 12, "manoj", "kumar", name = "kumar", age = "22")
