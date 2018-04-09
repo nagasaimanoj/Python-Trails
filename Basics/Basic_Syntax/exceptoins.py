acee4d2297e88902eb366a4fae77de2df34a767b
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-04-09 11:59:02 +0530

    Create exceptoins.py

---
+++ Basics/Basic_Syntax/exceptoins.py
@@ -0,0 +1,35 @@
+class Animal:
+  def sound(self):
+    raise NotImplementedError("Logic Not Implemented")
+
+  def name(self):
+    raise NotImplementedError("Logic Not Implemented")
+
+class Dog(Animal):
+  def sound(self): 
+    print("bow - bow")
+
+  def name(self):
+    print("scooby")
+
+try:
+  object = Animal()
+  object.sound()
+  object.name()
+except Exception as e:
+  print(e)
+else:
+  print("else block")
+finally:
+  print("finally block")
+
+try:
+  object = Dog()
+  object.sound()
+  object.name()
+except Exception as e:
+  print(e)
+else:
+  print("else block")
+finally:
+  print("finally block")
