1157e76608b8d7b59dbdbf2dc2eb4c5c5d89516c
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-04-09 10:54:19 +0530

    Create setter_getter.py

---
+++ Basics/Basic_Syntax/setter_getter.py
@@ -0,0 +1,20 @@
+class Person:
+  
+  @property
+  def val(self):
+    pass
+  
+  @val.setter
+  def val(self, val):
+    print("@val.setter called")
+    self._val = val
+    
+  @val.getter
+  def val(self):
+    print("@val.getter called")
+    return self._val
+
+
+p = Person()
+p.val = 30
+print(p.val)
