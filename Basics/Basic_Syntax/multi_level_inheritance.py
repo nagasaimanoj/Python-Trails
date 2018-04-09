1409f3b9fbd5c75a4ee30d6dfe2e3d9939868551
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-04-09 11:10:52 +0530

    Create multi_level_inheritance.py

---
+++ Basics/Basic_Syntax/multi_level_inheritance.py
@@ -0,0 +1,13 @@
+class grand_parent:
+  def __init__(self):
+    print("from grand_parent")
+
+class parent(grand_parent):
+  def __init__(self):
+    print("from parent")
+
+class child(parent):
+  def __init__(self):
+    print("from child")
+
+obj = child()
