1b7e765a25b3498184104ac687b072065a95d24d
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-04-12 14:16:53 +0530

    code re-formatted

--- Web Dev/Django/MySite/manage.py
+++ Web Dev/Django/MySite/manage.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 import os
 import sys
 
@@ -10,6 +9,5 @@ if __name__ == "__main__":
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+            "forget to activate a virtual environment?") from exc
     execute_from_command_line(sys.argv)
