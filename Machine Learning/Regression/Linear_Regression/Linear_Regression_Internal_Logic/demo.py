de25669a601a3ac13bd254804ede9a629209757b
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-04-12 14:26:03 +0530

    code re-formatted

--- Machine Learning/Regression/Linear_Regression/Linear_Regression_Internal_Logic/demo.py
+++ Machine Learning/Regression/Linear_Regression/Linear_Regression_Internal_Logic/demo.py
@@ -3,7 +3,8 @@ from os.path import dirname
 from numpy import (genfromtxt, mean)
 
 if __name__ == "__main__":
-    DATA_POINTS = genfromtxt(dirname(__file__) + "\\" + "data.csv", delimiter=",")
+    DATA_POINTS = genfromtxt(
+        dirname(__file__) + "\\" + "data.csv", delimiter=",")
 
     data_intercept, data_slope = 0, 0
 
@@ -12,8 +13,9 @@ if __name__ == "__main__":
     learning_rate = 1 / data_num_iterations / 10
 
     for i in range(data_num_iterations):
-        avg_change = mean(2 / len(DATA_POINTS) *
-                          (DATA_POINTS[:, 1] - data_slope * DATA_POINTS[:, 0] + data_intercept))
+        avg_change = mean(2 / len(DATA_POINTS) * (
+            DATA_POINTS[:, 1] - data_slope * DATA_POINTS[:, 0] + data_intercept
+        ))
 
         data_intercept += avg_change * learning_rate
 
@@ -21,5 +23,6 @@ if __name__ == "__main__":
 
     print("intercept =", data_intercept)
     print("slope =", data_slope)
-    print("average error =", mean(
-        (DATA_POINTS[:, 1] - (data_slope * DATA_POINTS[:, 0] + data_intercept)) ** 2))
+    print("average error =",
+          mean((DATA_POINTS[:, 1] -
+                (data_slope * DATA_POINTS[:, 0] + data_intercept))**2))
