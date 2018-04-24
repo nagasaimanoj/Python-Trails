c570791f7a8392b233c81efc0dca0389c259e676
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-04-24 18:52:30 +0530

    import conflicts handling

--- temp/import_conflict/impotr_conflict.py
+++ temp/import_conflict/impotr_conflict.py
@@ -1,5 +1,17 @@
 import os, imp
 
-for description in imp.get_suffixes():
-    abs_path = os.path.dirname(__file__)
-    imp.load_module("pkg", open(abs_path), abs_path, (description))
+
+def import_module(dir, name):
+    """ load a module (not a package) with a given name 
+        from the specified directory 
+    """
+    for description in imp.get_suffixes():
+        (suffix, mode, type) = description
+        if not suffix.startswith('.py'): continue
+        abs_path = os.path.join(dir, name + suffix)
+        if not os.path.exists(abs_path): continue
+        fh = open(abs_path)
+        return imp.load_module(name, fh, abs_path, (description))
+
+
+import_module('.', 'pkg')
