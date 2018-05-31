8c1fcdf591ed9d103c15f6b572db556a7f7e0058
Naga Sai Manoj Kumar Giddaluru -X (ngiddalu - HCL TECHNOLOGIES LIMITED at Cisco) <ngiddalu@partnet.cisco.com>
2018-05-31 14:35:40 +0530

    rectified : changed logic to reduce mistakes

--- Machine Learning/Regression/Linear_Regression/Linear_Regression_Internal_Logic/demo.py
+++ Machine Learning/Regression/Linear_Regression/Linear_Regression_Internal_Logic/demo.py
@@ -3,38 +3,47 @@ from os.path import dirname
 from matplotlib.pyplot import plot, scatter, show
 
 
-def print_values():
-    print("intercept =", b)
-    print("slope =", m)
-    print("error =", error)
-
-
 current_dir = dirname(__file__)
 data_set = genfromtxt(current_dir + "/data.csv", delimiter=",")
+x_list = data_set[:, 0]
+y_list = data_set[:, 1]
 
-x = data_set[:, 0]
-y = data_set[:, 1]
 b = 0
 m = 0
 
-predected_y = m*x+b
-error = mean(y - predected_y)
+predected_y = m * x_list + b
+avg_error = mean((y_list - predected_y) ** 2)
 
-print_values()
-scatter(x, y)
+print("initially : ")
+print("intercept =", b)
+print("slope =", m)
+print("error =", avg_error)
+print("----------")
 
-num_iterations = len(data_set) * 10
-learning_rate = 1 / (num_iterations * 10)
+N = len(x_list)
+num_iterations = N * 100
+learning_rate = 1 / num_iterations
 
-for i in range(num_iterations):
-    error = mean(y - (m*x+b))
-    avg_change = 2 / len(data_set) * error
-    correction = avg_change * learning_rate
+for _ in range(num_iterations):
+    predected_y = m * x_list + b
+    y_difference = y_list - predected_y
 
-    b += correction
-    m += sum(x) * correction
+    b_gradient = sum(2 / N * y_difference)
+    m_gradient = sum(2 / N * x_list * y_difference)
 
-print_values()
-plot(x, m*x+b)
+    b += learning_rate * b_gradient
+    m += learning_rate * m_gradient
+
+predected_y = m * x_list + b
+avg_error = mean((y_list - predected_y) ** 2)
+
+print("finally :")
+print("intercept =", b)
+print("slope =", m)
+print("error =", avg_error)
+print("----------")
+
+scatter(x_list, y_list)
+plot(x_list, predected_y)
 
 show()
