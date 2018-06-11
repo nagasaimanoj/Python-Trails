598339ad3f88e7771b7c44e8d3d44233ae0b3f38
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-06-11 11:40:15 +0530

    removing braces in conditions

--- Fun_Ones/Root_Finder.py
+++ Fun_Ones/Root_Finder.py
@@ -4,7 +4,7 @@ num = Decimal(input("enter num : "))
 root = Decimal(input("enter root : "))
 mid_val = 0
 
-if (root == 0):
+if root == 0:
     mid_val = 'undefined'
 else:
     start = 0
@@ -13,16 +13,16 @@ else:
     result = 0
     mid_val = 0
 
-    while (mid_val != (start + end) / 2):
+    while mid_val != (start + end) / 2:
         mid_val = (start + end) / 2
         result = mid_val ** root
 
-        if (result < num):
+        if result < num:
             start = mid_val
-        if (result > num):
+        if result > num:
             end = mid_val
 
-    if (mid_val == int(mid_val)):
+    if mid_val == int(mid_val):
         mid_val = int(mid_val)
 
 print(str(root) + "√" + str(num) + " = " + str(mid_val))
