eb46d9e5f6f07fcbec2c9e8a558719f0396a4830
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-07-23 02:47:52 +0530

    angular seperated nd simplified

--- Web Dev/Flask/flask_angualr/templates/scripts.js
+++ Web Dev/Flask/flask_angualr/templates/scripts.js
@@ -1,12 +1,6 @@
-var app = angular.module('myApp', []);
+let app = angular.module('myApp', []);
 app.controller('myCtrl', function ($scope, $http) {
-    $http.get("http://localhost:5000")
-        .then(function (response) {
-            $scope.names = response.data;
-        });
-});
-app.controller('urCtrl', function ($scope, $http) {
-    $http.get("http://person-crud.herokuapp.com/r/getAll")
+    $http.get("http://127.0.0.1:5000/data")
         .then(function (response) {
             $scope.names = response.data;
         });
