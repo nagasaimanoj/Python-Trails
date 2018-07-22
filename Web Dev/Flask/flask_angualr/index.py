4b6dda05875de8ced9dd583c1d3bfc7a104897da
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-07-23 02:48:13 +0530

    trying add/delete

--- Web Dev/Flask/flask_angualr/index.py
+++ Web Dev/Flask/flask_angualr/index.py
@@ -4,7 +4,13 @@ from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
-names = ['naga', 'sai', 'manoj']
+names = []
+
+names.append({'name': 'g', 'age': 24})
+names.append({'name': 'naga', 'age': 25})
+names.append({'name': 'sai', 'age': 26})
+names.append({'name': 'manoj', 'age': 27})
+names.append({'name': 'kumar', 'age': 28})
 
 
 @app.after_request
@@ -19,7 +25,31 @@ def after_requests(response):
 
 @app.route('/')
 def home_page():
+    return render_template('/home', names=dumps(names))
+
+
+@app.route('/data')
+def data():
     return dumps(names)
+
+
+@app.route('/add', methods=['GET'])
+def add():
+    uname = request.form['uname']
+    uage = request.form['uage']
+    names.append({'name': uname, 'age': uage})
+    return redirect('/')
+
+
+@app.route('/delete', methods=['GET'])
+def delete():
+    del_name = request.args.get('name')
+    index = 0
+    for each_record in names:
+        if each_record['name'] == del_name:
+            del names[index]
+        index += 1
+    return redirect('/')
 
 
 app.run()
