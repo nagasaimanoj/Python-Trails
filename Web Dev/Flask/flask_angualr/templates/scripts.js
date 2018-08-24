75ba624baee3a182151c4e4694064b2bd032a4f7
InnovationLab NewMacMini <innovationlab@InnovationLabNewMacMini.local>
2018-08-24 13:25:59 +0530

    outgrown to https://github.com/nagasaimanoj/Flask-CRUD-Angular

--- Web Dev/Flask/flask_angualr/templates/scripts.js
+++
@@ -1,7 +0,0 @@
-let app = angular.module('myApp', []);
-app.controller('myCtrl', function ($scope, $http) {
-    $http.get("http://127.0.0.1:5000/data")
-        .then(function (response) {
-            $scope.names = response.data;
-        });
-});
