a0e96f9d403113933442464071e717de68ff5f49
InnovationLab NewMacMini <innovationlab@InnovationLabNewMacMini.local>
2018-09-05 13:58:34 +0530

    simple crud using flask-jinja

---
+++ Web Dev/Flask/simple_crud/index.py
@@ -0,0 +1,34 @@
+from flask import Flask, redirect, render_template, request
+
+app = Flask(__name__)
+
+userList = []
+
+
+@app.route('/', methods=['GET'])
+def home_page():
+    return render_template('home.html', userList=userList)
+
+
+@app.route('/addItem', methods=['POST'])
+def addItem():
+    uname = request.form['uname']
+    userList.append(uname)
+    return redirect('/')
+
+
+@app.route('/deleteItem', methods=['GET'])
+def deleteItem():
+    name = request.args.get('name')
+    userList.remove(name)
+    return redirect('/')
+
+
+@app.route('/editItem', methods=['GET'])
+def editItem():
+    name = request.args.get('name')
+    userList.remove(name)
+    return render_template('home.html', userList=userList, value=name)
+
+
+app.run()
